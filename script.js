9f008247f278bca8abe9cb6fa6fccf2b66885253
bikramgiri <asgbrg.github@gmail.com>
2025-01-19 21:24:09 +0545

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateAge() {
+
+      var dob = document.getElementById("dob").value;
+
+      var today = new Date();
+
+      var birthDate = new Date(dob);
+
+      var age = today.getFullYear() - birthDate.getFullYear();
+
+      var m = today.getMonth() - birthDate.getMonth();
+
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+      }
+
+      document.getElementById("age").innerHTML = "You are " + age + " years old.";
+
+  }
